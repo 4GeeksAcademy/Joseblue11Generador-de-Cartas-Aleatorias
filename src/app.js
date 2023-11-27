5da66dab2c9c5a081dd2a1dd10074e38c73376e0
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2023-11-27 13:05:13 +0000

    modificacion

--- src/app.js
+++ src/app.js
@@ -1,41 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+const card = document.querySelector('.card');
 
-//Generacion de carta aleatoria cuando se acutliza la pagina.
-window.onload = function() {
-  //Crear arrays con los valores de la carta
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let icons = ["♦", "♥", "♠", "♣"];
+function generateRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomCard = Math.floor(Math.random() * 13) + 1;
 
-  //Crear los numeros aleatorios para seleccionar los valores
-  const randomValueNum = Math.floor(Math.random() * numbers.length);
-  const randomValueIcons = Math.floor(Math.random() * icons.length);
+  card.className = `card ${randomSuit}`;
 
-  //Extraccion de los elementos HTML
-  let numero = document.getElementById("numero");
-  let iconoSuperior = document.getElementById("icono-superior");
-  let iconoInferior = document.getElementById("icono-inferior");
+  card.querySelector('.center').textContent = randomCard;
+  
+  const suitsElements = card.querySelectorAll('.suit');
+  
+  suitsElements.forEach((suitElement) => {
+    suitElement.textContent = '';
+    suitElement.classList.remove('hearts', 'clubs', 'diamonds', 'spades');
+    suitElement.classList.add(randomSuit);
+    suitElement.textContent = suitElement.getAttribute('data-suit');
+  });
+}
 
-  //Asignacion de color negro a los iconos que lo requieren
-  if (icons[randomValueIcons] == "♠" || icons[randomValueIcons] == "♣") {
-    iconoSuperior.style.color = "black";
-    iconoInferior.style.color = "black";
-  } else {
-    iconoSuperior.style.color = "red";
-    iconoInferior.style.color = "red";
-  }
+generateRandomCard();
 
-  //Asignacion de valores aleatorios
-  numero.innerHTML = numbers[randomValueNum];
-  iconoSuperior.innerHTML = icons[randomValueIcons];
-  iconoInferior.innerHTML = icons[randomValueIcons];
-};
-
-//Generacion de carta aleatoria al presionar el boton.
-let botonGenerar = document.getElementById("boton-generar");
-botonGenerar.addEventListener("click", () => {
-  window.onload();
-});
