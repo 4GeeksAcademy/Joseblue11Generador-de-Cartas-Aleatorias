39da8d9c672e5c961bb2d713c956f0765aba9e56
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2023-11-28 21:23:36 +0000

    correciones

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
-const card = document.querySelector('.card');
+const suits = ["hearts", "clubs", "diamonds", "spades"];
+const card = document.querySelector(".card");
 
 function generateRandomCard() {
   const randomSuit = suits[Math.floor(Math.random() * suits.length)];
@@ -7,17 +7,16 @@ function generateRandomCard() {
 
   card.className = `card ${randomSuit}`;
 
-  card.querySelector('.center').textContent = randomCard;
-  
-  const suitsElements = card.querySelectorAll('.suit');
-  
-  suitsElements.forEach((suitElement) => {
-    suitElement.textContent = '';
-    suitElement.classList.remove('hearts', 'clubs', 'diamonds', 'spades');
+  card.querySelector(".center").textContent = randomCard;
+
+  const suitsElements = card.querySelectorAll(".suit");
+
+  suitsElements.forEach(suitElement => {
+    suitElement.textContent = "";
+    suitElement.classList.remove("hearts", "clubs", "diamonds", "spades");
     suitElement.classList.add(randomSuit);
-    suitElement.textContent = suitElement.getAttribute('data-suit');
+    suitElement.textContent = suitElement.getAttribute("data-suit");
   });
 }
 
 generateRandomCard();
-
